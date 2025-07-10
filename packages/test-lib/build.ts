36c00119d8fc3dcf59c460a59dce9cab9361d3ab
youaresoyoung <zero.so.jung@gmail.com>
2025-07-09 18:57:29 -0700

    :fire: remove test files

--- packages/test-lib/build.ts
+++
@@ -1,8 +0,0 @@
-import { buildWithConfig } from "@illog/esbuild-config";
-
-buildWithConfig({
-  entryPoints: ["src/index.ts"],
-  outdir: "dist",
-  formats: ["cjs", "esm"],
-  external: "auto",
-});
