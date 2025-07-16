8225adbb9f6904408c08f866ec0f60e11ea5256c
youaresoyoung <zero.so.jung@gmail.com>
2025-07-15 20:08:36 -0700

    :sparkles: export and build themes

---
+++ packages/themes/build.ts
@@ -0,0 +1,8 @@
+import { buildWithConfig } from "@illog/esbuild-config";
+
+buildWithConfig({
+  entryPoints: ["src/index.ts"],
+  outdir: "dist",
+  formats: ["cjs", "esm"],
+  external: "auto",
+});
