67945fac3f9be991bec9c3c4692c7e02e8dc3751
youaresoyoung <zero.so.jung@gmail.com>
2025-07-22 22:58:22 -0700

    :sparkles: implement responsive theme tokens and export

---
+++ packages/themes/src/tokens/responsive.ts
@@ -0,0 +1,19 @@
+import { primitive } from "./typography/primitive";
+
+export const responsive = {
+  mobile: {
+    width: 375,
+    rootFontSize: primitive.scale[3],
+    scale: 1.25,
+  },
+  tablet: {
+    width: 768,
+    rootFontSize: primitive.scale[3],
+    scale: 1,
+  },
+  desktop: {
+    width: 1200,
+    rootFontSize: primitive.scale[3],
+    scale: 1,
+  },
+};
