b4a371fe566d62f598080e7bc9618a4a53f74e79
Soyoung Jung <143086717+youaresoyoung@users.noreply.github.com>
2025-07-30 23:32:09 -0700

    [ILLOG-41] feat: create shadow, radius, typography themes and refactor exports to named syntax (#3)
    * :sparkles: feat: implement size theme tokens and export
    
    * :sparkles: implement typography  theme tokens and export
    
    * :sparkles: implement responsive theme tokens and export
    
    * :sparkles: implement theme styes and export
    
    * :recycle: change export style to named export

---
+++ packages/themes/src/styles/effect/blur.ts
@@ -0,0 +1,13 @@
+import { blur as blurSize } from "../../tokens/size/blur";
+
+export const blur = {
+  overlay: {
+    blur: blurSize[100],
+  },
+  layer: {
+    blur: blurSize[100],
+  },
+  glass: {
+    blur: blurSize[100],
+  },
+};
---
+++ packages/themes/src/tokens/size/depth.ts
@@ -0,0 +1,15 @@
+export const depth = {
+  0: 0,
+  25: 1,
+  100: 4,
+  200: 8,
+  400: 16,
+  800: 32,
+  1200: 48,
+  negative025: -1,
+  negative100: -4,
+  negative200: -8,
+  negative400: -16,
+  negative800: -32,
+  negative1200: -48,
+};
