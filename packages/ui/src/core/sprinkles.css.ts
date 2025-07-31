549296c4a6880b06e961a078c832c6023680ea21
youaresoyoung <zero.so.jung@gmail.com>
2025-07-30 18:41:00 -0700

    :sparkles: define margin and padding properties for ui packages

---
+++ packages/ui/src/core/sprinkles.css.ts
@@ -0,0 +1,39 @@
+import { createSprinkles, defineProperties } from "@vanilla-extract/sprinkles";
+import { tokens } from "packages/themes/dist";
+
+const space = tokens.size.space;
+
+export const marginAndPaddingProperties = defineProperties({
+  properties: {
+    margin: space,
+    marginTop: space,
+    marginRight: space,
+    marginBottom: space,
+    marginLeft: space,
+    padding: space,
+    paddingTop: space,
+    paddingRight: space,
+    paddingBottom: space,
+    paddingLeft: space,
+  },
+  shorthands: {
+    m: ["margin"],
+    mt: ["marginTop"],
+    mr: ["marginRight"],
+    mb: ["marginBottom"],
+    ml: ["marginLeft"],
+    mx: ["marginLeft", "marginRight"],
+    my: ["marginTop", "marginBottom"],
+    p: ["padding"],
+    pt: ["paddingTop"],
+    pr: ["paddingRight"],
+    pb: ["paddingBottom"],
+    pl: ["paddingLeft"],
+    px: ["paddingLeft", "paddingRight"],
+    py: ["paddingTop", "paddingBottom"],
+  },
+});
+
+export const sprinkles = createSprinkles(marginAndPaddingProperties);
+
+export type Sprinkles = Parameters<typeof sprinkles>[0];
