a3908757eda0a1531d59845c09ef1937ccc342c3
youaresoyoung <zero.so.jung@gmail.com>
2025-08-05 20:31:04 -0700

    :sparkles: create Button stories

---
+++ services/storybook/stories/UI/Button/Button.stories.tsx
@@ -0,0 +1,40 @@
+import { Button as _Button } from "@illog/ui";
+import { Meta, StoryObj } from "@storybook/react-vite";
+
+const meta = {
+  component: _Button,
+  argTypes: {
+    variant: {
+      control: {
+        type: "select",
+      },
+      options: ["primary", "secondary", "tertiary"],
+    },
+    size: {
+      control: {
+        type: "select",
+      },
+      options: ["sm", "md", "lg"],
+    },
+    isDisabled: {
+      control: {
+        type: "boolean",
+      },
+    },
+    isFullWidth: {
+      control: {
+        type: "boolean",
+      },
+    },
+  },
+} satisfies Meta<typeof _Button>;
+
+export default meta;
+type Story = StoryObj<typeof _Button>;
+
+export const Button: Story = {};
+Button.args = {
+  variant: "primary",
+  size: "md",
+  children: "Button",
+};
