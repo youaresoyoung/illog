59462bac73f95dbcee87049238dd444b4bd1e134
Soyoung Jung <143086717+youaresoyoung@users.noreply.github.com>
2025-08-08 22:23:30 -0700

    [ILLOG-42] feat: add foundational Button component with design token integration (#8)
    * :sparkles: create Button component
    
    * :sparkles: create Button stories
    
    * :hammer: remove the style prop
    
    * :hammer: add the aria label control to Button storybook
    
    * :hammer: add ButtonHTMLAttributes types to ButtonProps
    
    * :hammer: add reset properties at ButtonBsse

---
+++ packages/ui/src/components/Button/button.css.ts
@@ -0,0 +1,54 @@
+import { recipe } from "@vanilla-extract/recipes";
+import { styles, tokens } from "packages/themes/dist";
+
+const buttonBase = {
+  display: "inline-flex",
+  alignItems: "center",
+  justifyContent: "center",
+  borderRadius: tokens.size.radius[200],
+  cursor: "pointer",
+  border: "none",
+  textDecoration: "none",
+  backgroundColor: "transparent",
+  color: tokens.colors.light.text.default.default,
+  appearance: "none" as const,
+  transition: "all 0.2s ease-in-out",
+  ...styles.text.singleLine.bodyBase,
+};
+
+export const buttonRecipe = recipe({
+  base: buttonBase,
+  variants: {
+    size: {
+      sm: {},
+      md: { padding: tokens.size.space[300], height: "2.5rem" },
+      lg: {},
+    },
+    variant: {
+      primary: {
+        color: tokens.colors.light.text.brand.onBrand,
+        border: `${tokens.size.stroke.border}px solid ${tokens.colors.light.border.brand.default}`,
+        backgroundColor: tokens.colors.light.background.brand.default,
+
+        ":hover": {
+          color: tokens.colors.light.text.brand.onBrand,
+          backgroundColor: tokens.colors.light.background.brand.defaultHover,
+        },
+        ":disabled": {
+          cursor: "default",
+          color: tokens.colors.light.text.disabled.default,
+          backgroundColor: tokens.colors.light.background.disabled.default,
+          borderColor: tokens.colors.light.border.disabled.default,
+        },
+      },
+    },
+    isFullWidth: {
+      true: {
+        width: "100%",
+      },
+      false: {
+        width: "auto",
+      },
+    },
+  },
+});
