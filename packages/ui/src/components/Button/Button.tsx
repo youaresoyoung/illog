b949f310825a1727708a948cd37a405c4daa1b10
Soyoung Jung <143086717+youaresoyoung@users.noreply.github.com>
2025-08-08 22:23:30 -0700

    [ILLOG-42] feat: add foundational Button component with design token integration (#8)
    * :sparkles: create Button component
    
    * :sparkles: create Button stories
    
    * :hammer: remove the style prop
    
    * :hammer: add the aria label control to Button storybook
    
    * :hammer: add ButtonHTMLAttributes types to ButtonProps
    
    * :hammer: add reset properties at ButtonBsse

---
+++ packages/ui/src/components/Button/Button.tsx
@@ -0,0 +1,38 @@
+import clsx from "clsx";
+import { ButtonProps } from "./types";
+import { buttonRecipe } from "./button.css";
+
+export const Button = (props: ButtonProps) => {
+  const {
+    variant = "primary",
+    size = "md",
+    isDisabled,
+    onClick,
+    children,
+    className,
+    isFullWidth,
+    ariaLabel,
+    ref,
+    ...rest
+  } = props;
+
+  return (
+    <button
+      {...rest}
+      className={clsx([
+        buttonRecipe({
+          variant,
+          size,
+          isFullWidth,
+        }),
+        className,
+      ])}
+      aria-label={ariaLabel}
+      ref={ref}
+      onClick={onClick}
+      disabled={isDisabled}
+    >
+      {children}
+    </button>
+  );
+};
