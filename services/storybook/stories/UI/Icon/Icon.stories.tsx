e068316b8c682b7ab06165554c802ec18fa09c00
youaresoyoung <zero.so.jung@gmail.com>
2025-08-11 16:31:20 -0700

    :sparkles: create Icon component story

---
+++ services/storybook/stories/UI/Icon/Icon.stories.tsx
@@ -0,0 +1,36 @@
+import { tokens } from "@illog/themes";
+import { Icon as _Icon } from "@illog/ui";
+import { IconNameOptions } from "@illog/ui";
+import { Meta, StoryObj } from "@storybook/react-vite";
+import { getColorOptions } from "../../../utils/getColorOptions";
+
+const colors = getColorOptions(tokens.colors.light.icon);
+const meta = {
+  component: _Icon,
+  argTypes: {
+    name: {
+      control: "select",
+      options: IconNameOptions,
+    },
+    size: {
+      control: "select",
+      options: tokens.size.icon ? Object.keys(tokens.size.icon) : [],
+    },
+    color: {
+      control: "select",
+      options: colors,
+    },
+  },
+} satisfies Meta<typeof _Icon>;
+
+export default meta;
+type Story = StoryObj<typeof _Icon>;
+
+export const Icon: Story = {};
+Icon.args = {
+  name: "ic_check_24",
+  size: "medium",
+  color: "default.default",
+};
+
+tokens.colors.light.icon;
