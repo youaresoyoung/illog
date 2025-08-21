3641d044bee06261b2a65009538459780e9593ab
youaresoyoung <zero.so.jung@gmail.com>
2025-08-20 20:10:43 -0700

    :sparkles: add prettier and ESLint configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import tseslint from 'typescript-eslint'
+import eslintPluginReact from 'eslint-plugin-react'
+import eslintPluginReactHooks from 'eslint-plugin-react-hooks'
+import eslintPluginReactRefresh from 'eslint-plugin-react-refresh'
+import eslintConfigPrettier from 'eslint-config-prettier'
+
+export default tseslint.config(
+  { ignores: ['**/node_modules', '**/dist'] },
+  tseslint.configs.recommended,
+  eslintPluginReact.configs.flat.recommended,
+  eslintPluginReact.configs.flat['jsx-runtime'],
+  {
+    // TODO: 수동 명시가 아닌 모노레포 구조 안에서 패키지들의 react 버전을 자동으로 감지하도록 설정해야함
+    settings: {
+      react: {
+        version: '19.1.0'
+      }
+    }
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      'react-hooks': eslintPluginReactHooks,
+      'react-refresh': eslintPluginReactRefresh
+    },
+    rules: {
+      ...eslintPluginReactHooks.configs.recommended.rules,
+      ...eslintPluginReactRefresh.configs.vite.rules,
+      '@typescript-eslint/explicit-function-return-type': 'off'
+    }
+  },
+  eslintConfigPrettier
+)
