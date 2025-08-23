0255ab4e9f494606458e9aae15c367ccad08213b
youaresoyoung <zero.so.jung@gmail.com>
2025-08-23 12:24:55 -0700

    :construction: add basic CRUD functionality to UI

--- services/app/src/renderer/src/pages/Today.tsx
+++ services/app/src/renderer/src/pages/Today.tsx
@@ -1,3 +1,48 @@
+import { Task } from 'electron'
+import { useEffect, useState } from 'react'
+
 export const Today = () => {
-  return <div>Today</div>
+  const [tasks, setTasks] = useState<Task[]>([])
+
+  const handleCreateTask = async () => {
+    const task = await window.api.task.create({
+      title: 'Test Task',
+      status: 'todo'
+    })
+
+    setTasks((prev) => [...prev, task])
+  }
+
+  const handleDeleteTask = async (id: string) => {
+    await window.api.task.softDelete(id)
+    setTasks((prev) => prev.filter((task) => task.id !== id))
+  }
+
+  useEffect(() => {
+    async function getTasks() {
+      const tasks = await window.api.task.getAll()
+
+      setTasks(tasks)
+    }
+    getTasks()
+  }, [])
+
+  return (
+    <div>
+      [Today]
+      <button onClick={handleCreateTask}>Create Task</button>
+      {tasks && tasks.length > 0 ? (
+        <ul>
+          {tasks.map((task) => (
+            <li key={task.id}>
+              {task.title}
+              <button onClick={() => handleDeleteTask(task.id)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No tasks for today</p>
+      )}
+    </div>
+  )
 }
