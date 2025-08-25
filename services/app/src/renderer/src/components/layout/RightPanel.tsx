1ac64e653e9921d3b9de82c718b1b461691eacef
youaresoyoung <zero.so.jung@gmail.com>
2025-08-25 13:05:20 -0700

    :sparkles: apply auto save and debounce to UI

--- services/app/src/renderer/src/components/layout/RightPanel.tsx
+++ services/app/src/renderer/src/components/layout/RightPanel.tsx
@@ -1,4 +1,43 @@
+import { ChangeEvent, useEffect, useState } from 'react'
+import { useTaskStore } from '../../stores/taskStore'
+import { TaskNote } from 'services/app/src/types'
+
 export const RightPanel = () => {
+  const currentTaskId = useTaskStore((state) => state.currentTaskId)
+  const [note, setNote] = useState<TaskNote | null>(null)
+
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setNote((prev) => ({ ...prev, content: e.target.value }) as TaskNote)
+  }
+
+  useEffect(() => {
+    const timeoutId = setTimeout(async () => {
+      if (note?.content && currentTaskId) {
+        const result = await window.api.note.autoSave(currentTaskId, note.content, Date.now())
+
+        if (!result.conflict) {
+          setNote({
+            task_id: result.note.task_id,
+            content: result.note.content,
+            updated_at: result.note.updated_at
+          })
+        }
+      }
+    }, 1000)
+
+    return () => clearTimeout(timeoutId)
+  }, [note?.content, currentTaskId])
+
+  useEffect(() => {
+    if (currentTaskId) {
+      const getNote = async () => {
+        const note = await window.api.note.findByTaskId(currentTaskId)
+        setNote(note || null)
+      }
+      getNote()
+    }
+  }, [currentTaskId])
+
   return (
     <div
       style={{
@@ -7,7 +46,12 @@ export const RightPanel = () => {
         padding: '12px'
       }}
     >
-      RightPanel
+      <textarea
+        placeholder="Type your notes here..."
+        style={{ width: '100%', height: '100%' }}
+        value={note?.content || ''}
+        onChange={handleChange}
+      />
     </div>
   )
 }
