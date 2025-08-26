d0c4219a3c312d275ec3c744bad6d6e36dcda73d
youaresoyoung <zero.so.jung@gmail.com>
2025-08-25 23:02:11 -0700

    :sparkles: apply tag name update to UI

--- services/app/src/renderer/src/components/tag/TagEditor.tsx
+++ services/app/src/renderer/src/components/tag/TagEditor.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react'
+import { ChangeEvent, useEffect, useRef, useState } from 'react'
 import { useTagStore } from '../../stores/useTagStore'
 import { Color } from 'services/app/src/types'
 
@@ -10,18 +10,37 @@ type Props = {
 export const TagEditor = ({ id, name }: Props) => {
   const { deleteTag, updateTag } = useTagStore()
 
-  const colorSelect = useRef<HTMLSelectElement>(null)
+  const [tagName, setTagName] = useState(name)
+  const inputSelectRef = useRef<HTMLInputElement>(null)
+  const colorSelectRef = useRef<HTMLSelectElement>(null)
 
   const handleChangeColor = () => {
-    const color = colorSelect.current?.value as Color
+    const color = colorSelectRef.current?.value as Color
     updateTag(id, { color })
   }
 
+  const handleChangeName = (e: ChangeEvent<HTMLInputElement>) => {
+    setTagName(e.currentTarget.value)
+  }
+
+  useEffect(() => {
+    const onClickOutSide = (e: MouseEvent) => {
+      if (e.currentTarget === inputSelectRef.current) return
+      if (name !== inputSelectRef.current?.value) {
+        updateTag(id, { name: inputSelectRef.current?.value })
+      }
+    }
+
+    document.addEventListener('mousedown', onClickOutSide)
+
+    return () => document.removeEventListener('mousedown', onClickOutSide)
+  }, [])
+
   return (
     <div>
-      <input value={name} />
+      <input value={tagName} ref={inputSelectRef} onChange={handleChangeName} />
       <button onClick={() => deleteTag(id)}>delete</button>
-      <select name="color" id="color" ref={colorSelect} onChange={handleChangeColor}>
+      <select name="color" id="color" ref={colorSelectRef} onChange={handleChangeColor}>
         {['blue', 'green', 'yellow', 'purple', 'red', 'gray'].map((color) => (
           <option key={color}>{color}</option>
         ))}
