342735b56b60db2d459f08e4510bf04670a46103
youaresoyoung <zero.so.jung@gmail.com>
2025-08-29 15:37:17 -0700

    :sparkles: apply nav component to Left panel (renderer)

--- services/app/src/renderer/src/components/layout/LeftPanel.tsx
+++ services/app/src/renderer/src/components/layout/LeftPanel.tsx
@@ -1,19 +1,30 @@
-import { Link } from 'react-router'
+import { Navigation, Text, useNavContext } from '@illog/ui'
 
 export const LeftPanel = () => {
+  const { activeId, list, updateActiveId } = useNavContext()
+
+  const handleItemClick = (id: string) => {
+    updateActiveId(id)
+  }
+
   return (
-    <nav
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        width: '256px',
-        borderRight: '1px solid #ccc',
-        padding: '12px'
-      }}
-    >
-      <Link to="/">Today</Link>
-      <Link to="/this-week">This Week</Link>
-      <Link to="/reflection">Reflection</Link>
-    </nav>
+    <Navigation.Container>
+      <Text as="h1" textStyle="heading">
+        illog
+      </Text>
+      <Navigation.List mt="1200">
+        {list.map((page) => (
+          <Navigation.Item
+            key={page.id}
+            id={page.id}
+            to={page.to}
+            iconName={page.iconName}
+            label={page.label}
+            onClick={() => handleItemClick(page.id)}
+            isActive={activeId === page.id}
+          />
+        ))}
+      </Navigation.List>
+    </Navigation.Container>
   )
 }
