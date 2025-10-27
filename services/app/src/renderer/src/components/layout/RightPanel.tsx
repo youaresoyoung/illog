2a8316e1d36eecdf4f766f4122c05b1125c0ab18
youaresoyoung <zero.so.jung@gmail.com>
2025-10-27 11:40:05 -0700

    :sparkles: add task's title input to the RightPanel

--- services/app/src/renderer/src/components/layout/RightPanel.tsx
+++ services/app/src/renderer/src/components/layout/RightPanel.tsx
@@ -1,9 +1,26 @@
+import { useEffect } from 'react'
 import { useAutoSaveNote } from '../../hooks/useAutoSaveNote'
 import { useUIStoreState } from '../../stores/useUIStore'
+import { useTask, useUpdateTask } from '../../hooks/queries'
+import { Input, useAutoSaveInput } from '@illog/ui'
 
 export const RightPanel = () => {
   const { currentTaskId } = useUIStoreState()
+  const { data: task } = useTask(currentTaskId!)
   const [note, handleChange] = useAutoSaveNote(currentTaskId)
+
+  const { mutate: updateTask } = useUpdateTask()
+  const [title, setTitle, handleTitleChange] = useAutoSaveInput(
+    task?.title || '',
+    (value) => updateTask({ id: task!.id, contents: { title: value } }),
+    1000
+  )
+
+  useEffect(() => {
+    if (task?.title !== undefined) {
+      setTitle(task.title)
+    }
+  }, [task?.title, setTitle])
 
   return (
     <div
@@ -13,6 +30,7 @@ export const RightPanel = () => {
         padding: '12px'
       }}
     >
+      <Input value={title} onChange={handleTitleChange} />
       <textarea
         placeholder="Type your notes here..."
         style={{ width: '100%', height: '100%' }}
