2dbe0a5b02ebf0d2c8ca941ed27461a7df7335db
youaresoyoung <zero.so.jung@gmail.com>
2025-10-27 12:15:18 -0700

    :sparkles: install "@blackglory/better-sqlite3-migrations" and set up the db migration

---
+++ services/app/src/main/database/migrations.ts
@@ -0,0 +1,16 @@
+import { readFileSync } from 'fs'
+import { join } from 'path'
+import { isDev } from '../../utils/utils'
+import { app } from 'electron'
+
+const migrationsPath = isDev()
+  ? join(app.getAppPath(), 'src/main/database/migrations')
+  : join(process.resourcesPath, 'migrations')
+
+export const migrations = [
+  {
+    version: 1,
+    up: readFileSync(join(migrationsPath, '001_initial_schema.sql'), 'utf8'),
+    down: ''
+  }
+]
