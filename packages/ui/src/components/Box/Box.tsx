6425a70fa96db7677757614d04b7ac0f11e0d39c
youaresoyoung <zero.so.jung@gmail.com>
2025-11-01 16:49:56 -0700

    :hammer: [ui-package] enable styleProps on Box component

--- packages/ui/src/components/Box/Box.tsx
+++ packages/ui/src/components/Box/Box.tsx
@@ -3,17 +3,28 @@ import { BoxProps } from './types'
 import { extractSprinkleProps } from '../../utils/util'
 import { sprinkles } from '../../core/sprinkles.css'
 import clsx from 'clsx'
+import { convertStylePropsToCSS, extractStyleProps } from '../../core/styleProps'
 
+/**
+ * Box - A versatile container component that applies styling and layout properties.
+ */
 export const Box = <T extends ElementType>(props: BoxProps<T>) => {
-  const { as = 'div', className, children } = props
+  const { as = 'div', className, children, style } = props
 
-  const [sprinkleProps, restProps] = extractSprinkleProps(props)
+  const [styleProps, withoutStyleProps] = extractStyleProps(props)
+  const [sprinkleProps, restProps] = extractSprinkleProps(withoutStyleProps)
+
+  const mergedStyle = {
+    ...convertStylePropsToCSS(styleProps),
+    ...style
+  }
 
   return createElement(
     as,
     {
       ...restProps,
-      className: clsx([sprinkles(sprinkleProps), className])
+      className: clsx([sprinkles(sprinkleProps), className]),
+      style: mergedStyle
     },
     children
   )
