cb1a58bbdd428e5a2f4a423de7abaf9ab1f42dc4
youaresoyoung <zero.so.jung@gmail.com>
2025-11-02 16:25:31 -0800

    update style

--- services/app/src/renderer/src/components/layout/RightPanel.tsx
+++ services/app/src/renderer/src/components/layout/RightPanel.tsx
@@ -1,16 +1,18 @@
 import { useAutoSaveNote } from '../../hooks/useAutoSaveNote'
 import { useReflection, useReflectionStream, useTask, useUpdateTask } from '../../hooks/queries'
 import {
-  Box,
   Button,
   Dialog,
+  Inline,
   Input,
   Stack,
+  Text,
   TimePicker,
   useAutoSaveInput,
   useDialog
 } from '@illog/ui'
 import { TagSection } from '../tag/TagSection'
+import { MAX_TAG_LENGTH } from '../../../../types'
 
 type Props = {
   taskId: string
@@ -80,59 +82,88 @@ export const RightPanel = ({ taskId }: Props) => {
   }
 
   return (
-    <Box
-      width="594px"
-      height="100vh"
-      overflow="scroll"
-      borderLeft="border"
-      borderLeftColor="borderDefaultDefault"
-      borderLeftStyle="solid"
-      p="300"
-      backgroundColor="backgroundDefaultDefault"
-    >
-      <Input value={title} onChange={handleTitleChange} />
-      <Input value={description} onChange={handleDescriptionChange} />
-      <div>
-        <TagSection task={task} />
-        <TimePicker
-          value={{ start: task.timer_start ?? null, end: task.timer_end ?? null }}
-          onChange={handleDateTimeChange}
-        >
-          <TimePicker.Range>
-            <TimePicker.Input field="start" placeholder="Start time" />
-            <TimePicker.Separator />
-            <TimePicker.Input field="end" placeholder="End time" />
-          </TimePicker.Range>
-          <TimePicker.Summary showTime={false} />
-        </TimePicker>
-      </div>
-      <Button
-        variant="primary"
-        size="md"
-        onClick={handleReflectNote}
-        isDisabled={isStreaming || !note?.content}
+    <>
+      <Stack
+        position="fixed"
+        top="0"
+        bottom="0"
+        right="0"
+        width="720px"
+        // height="100vh"
+        overflow="scroll"
+        borderLeft="border"
+        borderLeftColor="borderDefaultDefault"
+        borderLeftStyle="solid"
+        p="300"
+        backgroundColor="backgroundDefaultDefault"
+        gap="1200"
       >
-        {isStreaming ? 'Generating...' : 'AI Reflection'}
-      </Button>
-      <textarea
-        placeholder="Type your notes here..."
-        style={{ width: '100%', height: '400px' }}
-        value={note?.content || ''}
-        onChange={handleChange}
-      />
-      <div>
-        <h4>AI Reflection</h4>
-        <Stack
-          as="p"
-          height="320px"
-          bg="backgroundDefaultSecondary"
-          p="200"
-          mt="400"
-          overflow="auto"
-        >
-          {displayReflectionText}
+        <Stack>
+          <Input value={title} onChange={handleTitleChange} />
+          <Text textStyle="caption" mt="100">
+            Last edited at {new Date(task?.updated_at).toLocaleString()}
+          </Text>
+          {/* <Input value={description} onChange={handleDescriptionChange} /> */}
         </Stack>
-      </div>
+        <Inline>
+          <Stack gap="400" flex="1">
+            <Inline>
+              <Text textStyle="bodyStrong">Tags</Text>
+              <Text textStyle="captionStrong" color="textDefaultTertiary" px="300" py="100">
+                {task.tags.length}/{MAX_TAG_LENGTH} used
+              </Text>
+            </Inline>
+            <TagSection task={task} />
+          </Stack>
+          <Stack flex="1" gap="400">
+            <Text textStyle="bodyStrong">Time</Text>
+            <TimePicker
+              value={{ start: task.timer_start ?? null, end: task.timer_end ?? null }}
+              onChange={handleDateTimeChange}
+            >
+              <TimePicker.Range>
+                <TimePicker.Input field="start" placeholder="Start time" />
+                <TimePicker.Separator />
+                <TimePicker.Input field="end" placeholder="End time" />
+              </TimePicker.Range>
+              <TimePicker.Summary showTime={false} />
+            </TimePicker>
+          </Stack>
+        </Inline>
+
+        <Stack>
+          <textarea
+            placeholder="Type your notes here..."
+            style={{ width: '100%', height: '400px' }}
+            value={note?.content || ''}
+            onChange={handleChange}
+          />
+        </Stack>
+        <Stack>
+          <Button
+            variant="primary"
+            size="md"
+            onClick={handleReflectNote}
+            isDisabled={isStreaming || !note?.content}
+          >
+            {isStreaming ? 'Generating...' : 'AI Reflection'}
+          </Button>
+          <div>
+            <h4>AI Reflection</h4>
+            <Stack
+              as="p"
+              height="320px"
+              bg="backgroundDefaultSecondary"
+              p="200"
+              mt="400"
+              overflow="auto"
+            >
+              <p>{displayReflectionText}</p>
+            </Stack>
+          </div>
+        </Stack>
+      </Stack>
+
       <Dialog isOpen={isDialogOpen} onClose={closeDialog}>
         <Dialog.Title>Do you want to delete the previous summary?</Dialog.Title>
         <Dialog.Description>
@@ -147,6 +178,6 @@ export const RightPanel = ({ taskId }: Props) => {
           </Button>
         </Dialog.Footer>
       </Dialog>
-    </Box>
+    </>
   )
 }
