441210d92827922b2f8ea3234ab96804083ad1a8
youaresoyoung <zero.so.jung@gmail.com>
2025-11-04 19:37:30 -0800

    :art: apply code formatting via pnpm format

--- packages/ui/src/components/Icon/types.ts
+++ packages/ui/src/components/Icon/types.ts
@@ -1,19 +1,32 @@
 // ⚠️ Auto-generated file - Do not edit
 
-export type IconName = 'calendar_today' | 'calendar_week' | 'cancel' | 'check' | 'chevron_down' | 'clock' | 'folder' | 'menu' | 'more' | 'plus' | 'setting' | 'tag' | 'trash';
+export type IconName =
+  | 'calendar_today'
+  | 'calendar_week'
+  | 'cancel'
+  | 'check'
+  | 'chevron_down'
+  | 'clock'
+  | 'folder'
+  | 'menu'
+  | 'more'
+  | 'plus'
+  | 'setting'
+  | 'tag'
+  | 'trash'
 
 export const IconNameOptions: IconName[] = [
-  "calendar_today",
-  "calendar_week",
-  "cancel",
-  "check",
-  "chevron_down",
-  "clock",
-  "folder",
-  "menu",
-  "more",
-  "plus",
-  "setting",
-  "tag",
-  "trash"
-];
+  'calendar_today',
+  'calendar_week',
+  'cancel',
+  'check',
+  'chevron_down',
+  'clock',
+  'folder',
+  'menu',
+  'more',
+  'plus',
+  'setting',
+  'tag',
+  'trash'
+]
