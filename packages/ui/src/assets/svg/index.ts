8928a0d8915d5d6089cab46bbde7b29be1c873d6
youaresoyoung <zero.so.jung@gmail.com>
2026-01-17 23:00:00 +0900

    [ILLOG-125] feat: add text formatting icons to icon set
    - Add SVG icons: bold, italic, underline, strikethrough
    - Add code, subscript, superscript icons
    - Update icon index and types for new icons

--- packages/ui/src/assets/svg/index.ts
+++ packages/ui/src/assets/svg/index.ts
@@ -1,31 +1,45 @@
 // ⚠️ Auto-generated file - Do not edit
 
+import bold from './ic_bold_24.svg'
 import calendar_today from './ic_calendar_today_24.svg'
 import calendar_week from './ic_calendar_week_24.svg'
 import cancel from './ic_cancel_24.svg'
 import check from './ic_check_24.svg'
 import chevron_down from './ic_chevron_down_24.svg'
 import clock from './ic_clock_24.svg'
+import code from './ic_code_24.svg'
 import folder from './ic_folder_24.svg'
+import italic from './ic_italic_24.svg'
 import menu from './ic_menu_24.svg'
 import more from './ic_more_24.svg'
 import plus from './ic_plus_24.svg'
 import setting from './ic_setting_24.svg'
+import strikethrough from './ic_strikethrough_24.svg'
+import subscript from './ic_subscript_24.svg'
+import superscript from './ic_superscript_24.svg'
 import tag from './ic_tag_24.svg'
 import trash from './ic_trash_24.svg'
+import underline from './ic_underline_24.svg'
 
 export default {
+  bold,
   calendar_today,
   calendar_week,
   cancel,
   check,
   chevron_down,
   clock,
+  code,
   folder,
+  italic,
   menu,
   more,
   plus,
   setting,
+  strikethrough,
+  subscript,
+  superscript,
   tag,
-  trash
+  trash,
+  underline
 }
--- packages/ui/src/components/Icon/types.ts
+++ packages/ui/src/components/Icon/types.ts
@@ -1,32 +1,46 @@
 // ⚠️ Auto-generated file - Do not edit
 
 export type IconName =
+  | 'bold'
   | 'calendar_today'
   | 'calendar_week'
   | 'cancel'
   | 'check'
   | 'chevron_down'
   | 'clock'
+  | 'code'
   | 'folder'
+  | 'italic'
   | 'menu'
   | 'more'
   | 'plus'
   | 'setting'
+  | 'strikethrough'
+  | 'subscript'
+  | 'superscript'
   | 'tag'
   | 'trash'
+  | 'underline'
 
 export const IconNameOptions: IconName[] = [
+  'bold',
   'calendar_today',
   'calendar_week',
   'cancel',
   'check',
   'chevron_down',
   'clock',
+  'code',
   'folder',
+  'italic',
   'menu',
   'more',
   'plus',
   'setting',
+  'strikethrough',
+  'subscript',
+  'superscript',
   'tag',
-  'trash'
+  'trash',
+  'underline'
 ]
