8dd2b7881347ad98678cd94f6578688c4c1e42e7
youaresoyoung <zero.so.jung@gmail.com>
2026-02-14 19:45:00 +0900

    refactor: extract DonutChart and TimeDistribution to @illog/ui components
    Replace inline SVG/layout logic with shared UI library components.

--- services/app/src/renderer/src/components/this-week/TimeDistribution.tsx
+++ services/app/src/renderer/src/components/this-week/TimeDistribution.tsx
@@ -1,68 +1,32 @@
-import { Box, Inline, Stack, Text } from '@illog/ui'
-import { DonutChart } from './DonutChart'
+import { TimeDistribution as UITimeDistribution } from '@illog/ui'
 import { formatHoursDecimal } from '../../utils/this-week-stats'
 import { DONUT_BACKGROUND_COLORS, DONUT_BORDER_COLORS } from '../../constant/color'
-import { ChartSegment } from '../../utils/category-analytics'
+import type { ChartSegment } from '../../utils/category-analytics'
 
 type Props = {
   segments: ChartSegment[]
-  isLeaf: boolean
-  onSegmentClick: (segment: ChartSegment) => void
+  isLeaf?: boolean
+  onSegmentClick?: (segment: ChartSegment) => void
 }
 
 export const TimeDistribution = ({ segments, isLeaf, onSegmentClick }: Props) => {
-  const isClickable = !isLeaf
+  const isClickable = !isLeaf && typeof onSegmentClick === 'function'
 
-  if (segments.length === 0) {
-    return (
-      <Stack gap="400">
-        <Text textStyle="bodyStrong" color="textDefaultDefault">
-          Time Distribution
-        </Text>
-        <Text textStyle="bodyBase" color="textDefaultTertiary">
-          No time data available
-        </Text>
-      </Stack>
-    )
-  }
   return (
-    <Stack gap="400">
-      <Text textStyle="bodyStrong" color="textDefaultDefault">
-        Time Distribution
-      </Text>
-      <Stack gap="600" align="center" bg="backgroundDefaultDefault" rounded="200" px="400" py="800">
-        <DonutChart segments={segments} />
-        <Inline gap="200" wrap="wrap" justify="center">
-          {segments.map((segment) => (
-            <Inline
-              maxWidth={240}
-              key={segment.id}
-              gap="200"
-              align="center"
-              style={{ cursor: isClickable ? 'pointer' : 'default' }}
-              onClick={isClickable ? () => onSegmentClick(segment) : undefined}
-              overflow="hidden"
-            >
-              {/* TODO: Create Dot UI component */}
-              <Box
-                flexShrink={0}
-                w="12px"
-                h="12px"
-                rounded="full"
-                style={{
-                  backgroundColor:
-                    DONUT_BACKGROUND_COLORS[segment.color] || DONUT_BACKGROUND_COLORS.gray,
-                  border: `1px solid ${DONUT_BORDER_COLORS[segment.color] || DONUT_BORDER_COLORS.gray}`
-                }}
-              />
-              <Text textStyle="caption" color="textDefaultSecondary" truncate="true">
-                {segment.name} ({formatHoursDecimal(segment.totalMinutes)},{' '}
-                {segment.percentage.toFixed(0)}%)
-              </Text>
-            </Inline>
-          ))}
-        </Inline>
-      </Stack>
-    </Stack>
+    <UITimeDistribution
+      title="Time Distribution"
+      items={segments}
+      getKey={(segment) => segment.id}
+      getValue={(segment) => segment.totalMinutes}
+      getFillColor={(segment) =>
+        DONUT_BACKGROUND_COLORS[segment.color] ?? DONUT_BACKGROUND_COLORS.gray
+      }
+      getStrokeColor={(segment) => DONUT_BORDER_COLORS[segment.color] ?? DONUT_BORDER_COLORS.gray}
+      getLabel={(segment, context) =>
+        `${segment.name} (${formatHoursDecimal(segment.totalMinutes)}, ${context.percentage.toFixed(0)}%)`
+      }
+      emptyMessage="No time data available"
+      onItemClick={isClickable ? (segment) => onSegmentClick?.(segment) : undefined}
+    />
   )
 }
